0da67889767326d77b42c733c085a5295588d7e5
Jourdan Neal <jjjet1991@gmail.com>
2021-03-21 19:55:39 -0700

    updating js file

--- theme.js
+++ theme.js
@@ -11,18 +11,24 @@ function setDefaultTheme()
     document.body.classList.value = theme;//Setting theme to the body classList value.
     document.getElementById( "icon-mode").classList.add(iconMode);//Add iconMode to classList.
     document.getElementById( "icon-text").innerHTML = iconText;//Bind iconText variable to icon-text HTML. 
+    
 }
 
 function setSelectedTheme()
 {
-    document.body.classList.value = localStorage.getItem("mode") || "light-theme";   
+    document.body.classList.value = localStorage.getItem("mode") || "light-theme"; 
 }
 
 function toggleMode(x)//Toggle mode, on click event. Toggle body classList from "light-theme" to "dark-theme".
 {
     let colorTheme = document.body.classList;//get the body's CSS class
-    let iconMode = x.classList
+    let iconMode = x.classList;
 
-    document.body.classList.toggle("dark-theme");//Toggle body.ClassList from light (default) to dark.
+
+    colorTheme.toggle("dark-theme");//Toggle document.body.ClassList (colorTheme) from light (default) to dark.
+    iconMode.toggle("fa-toggle-on");//Toggle iconMode (x.classList) to fa-toggle-on.
+    document.getElementById ("icon-text").innerHTML= "Dark Mode";
+ 
 }
+
     
