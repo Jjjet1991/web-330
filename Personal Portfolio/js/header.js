ae543907d85f8d5053d5ec4963bb69a1fb6b96e6
Jourdan Neal <jjjet1991@gmail.com>
2021-05-15 02:38:21 -0700

    updating personal portfolio

--- Personal Portfolio/js/header.js
+++ Personal Portfolio/js/header.js
@@ -7,50 +7,18 @@ super();
 
 connectedCallback() {
 this.innerHTML = `
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Jourdan Neal</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-
-      <li class="nav-item">
-        <a class="nav-link" href="index.html">Home</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="projects.html">Projects</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="#">Database Diagrams</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="#">API Unit Test</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="resume.html">Resume</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="about.html">About</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="bucket-list.html">Bucket List</a>
-      </li>
-
-
-    </ul>
-
-  </div>
+<nav class="nav justify-content-center" id="navbar">
+<a class="header nav-link active" aria-current="page" href="index.html">Home</a>
+<a class="header nav-link" href="./about.html">About</a>
+<a class="header nav-link" href="#">Resume</a>
+<a class="header nav-link" href="./bucket-list.html">Bucket List</a>
+<a class="header nav-link" href="./projects.html">Projects</a>
+<a class="header nav-link" href="#">Databases</a>
+<a class="header nav-link" href="#">API Tests</a>
 </nav>
 
 `;
+
 }
 }
 
